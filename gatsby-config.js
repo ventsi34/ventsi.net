4a3d75f8f2a3d7a49299488ff124e632fdfcbe6e
Ventsislav Dimitrov <ventsi34@gmail.com>
2020-04-28 21:45:10 +0300

    added social link animations, background images and fixing windows build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,41 @@
 module.exports = {
-    plugins: [
-        'gatsby-plugin-eslint',
-        'gatsby-plugin-sass',
-        {
-          resolve: 'gatsby-plugin-web-font-loader',
-          options: {
-            google: {
-              families: ['Baloo Paaji 2'],
-            }
-          }
-        },
-    ]
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `Ventsislav Dimitrov`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-eslint',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Baloo Paaji 2'],
+        }
+      }
+    },
+  ]
 }
